805b5f54da357e11b2fef0efceb4566994aed31d
Doug Daniels <daniels.douglas@gmail.com>
2013-02-04 19:20:34 -0600

    using winston logging and express access logging for nodejitsu deploy

--- example/server.js
+++ example/server.js
@@ -1,18 +1,30 @@
-var app = require('express').createServer();
+var express = require('express');
+var app = express.createServer();
 app.listen(8000);
 var webRTC = require('webrtc.io').listen(app);
+var expressWinston = require('express-winston');
+var winston = require('winston'); // for transports.Console
 
-
+app.use(expressWinston.logger({
+     transports: [
+       new winston.transports.Console({
+         json: true,
+         colorize: true
+       })
+     ]
+   }));
 
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
 app.get('/style.css', function(req, res) {
+	winston.info("style.css REQUESTED");
   res.sendfile(__dirname + '/style.css');
 });
 
 app.get('/webrtc.io.js', function(req, res) {
+	winston.info("webrtc.io.js REQUESTED");
   res.sendfile(__dirname + '/webrtc.io.js');
 });
 
@@ -54,3 +66,13 @@ webRTC.rtc.on('chat_msg', function(data, socket) {
     }
   }
 });
+
+app.use(expressWinston.errorLogger({
+      transports: [
+        new winston.transports.Console({
+          json: true,
+          colorize: true
+        })
+      ]
+    }));
+
